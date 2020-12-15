87c8b8d5a5df1467d24514b0f6df48d019199c81
Zanxus <Frederiksen_33@hotmai.com>
2020-12-15 06:33:08 +0100

    init transfer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static float highScore;
+    private float travelScore;
+    public static float pickUpScore;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Camera mainCamera;
+    Vector2 startPos;
+    Vector2 currentPos;
+    public TextMeshProUGUI HighscoreGUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y,-10);
+        startPos = player.transform.position;
+        currentPos = startPos;
+        travelScore = 0;
+        pickUpScore = 0;
+        highScore = 0;
+        HighscoreGUI.text = $"HighScore:\n{highScore}";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mainCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y,-10);
+        HighscoreGUI.text = $"HighScore:\n{highScore}";
+        currentPos = player.transform.position;
+
+        travelScore = Mathf.Floor(Vector2.Distance(startPos, currentPos));
+        highScore = travelScore + pickUpScore;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float JumpForce;
+    private float MoveInput;
+
+    private Rigidbody2D rb;
+    private Collider2D collider;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int maxExtraJumps; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        extraJumps = maxExtraJumps;
+    }
+
+    private void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius,whatIsGround);
+
+
+        MoveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(MoveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && MoveInput > 0)
+        {
+            Flip();
+        } else if(facingRight == true && MoveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * JumpForce;
+            extraJumps--;
+        }else if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * JumpForce;
+        }
+
+        if(isGrounded == true)
+        {
+            extraJumps = maxExtraJumps;
+        }
+
+        if(rb.velocity.y > 0.01)
+        {
+            Physics.IgnoreLayerCollision(1, 1, true);
+        }
+        else
+        {
+            Physics.IgnoreLayerCollision(9, 8, false);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+}
